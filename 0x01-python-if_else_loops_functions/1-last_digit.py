4b7c0d78fd98ece8efb8f9a88c125da1b4e4bd4b
MaeregSahilu <maereg.sahilu.a@gmail.com>
2023-05-11 21:27:14 +0300

    explain like im five

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -3,7 +3,7 @@ import random
 number = random.randint(-10000, 10000)
 num_str = repr(number)
 last_digit = int(num_str[-1])
-if last_digit < 6:
+if last_digit > 6:
     abv_five = "and is greater than 5"
 elif last_digit == 0:
     abv_five = "and is 0"
