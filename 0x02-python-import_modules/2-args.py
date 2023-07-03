b871318584a33d0e7c0f53c21ae46df1bf180328
MaeregSahilu <maereg.sahilu.a@gmail.com>
2023-07-03 15:08:16 +0300

    args

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    if len(sys.argv) == 1:
+        print("0 argumets:".format(len(sys.argv)))
+    elif len(sys.argv) == 2:
+        print("1 argument: \n 1: {}".format(sys.argv[1]))
+    else:
+        print("{} arguments:".format(len(sys.argv)-1))
+        n = 1
+        while n < len(sys.argv):
+            print("{}: {}".format(n, sys.argv[n]))
+            n += 1
