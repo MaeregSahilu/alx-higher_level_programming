2dbaa49461952221a5a63603122f362b46f80652
MaeregSahilu <maereg.sahilu.a@gmail.com>
2023-07-04 10:50:18 +0300

    sorted

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -3,6 +3,7 @@ if __name__ == "__main__":
     import hidden_4.pyc as hidden_4
 
     names = dir(hidden_4)
+    names.sort()
     for name in names:
         if name[:2] != "__":
             print(name)
